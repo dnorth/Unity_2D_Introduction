0c421242764b9fb5aa7d1e1d5b8f4338920433eb
Danny North <lds.north@gmail.com>
2017-04-19 23:03:25 -0600

    Add initial files

---
+++ 2D_Intro/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator animator;
+    private Rigidbody2D rigidBody;
+
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();    	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector2 move = new Vector2(Input.GetAxisRaw("Horizontal") * 2.0f, Input.GetAxisRaw("Vertical"));
+
+        if (move != Vector2.zero)
+        {
+            rigidBody.AddForce(move);
+            animator.SetTrigger("playerWalking");
+        }
+
+    }
+}
