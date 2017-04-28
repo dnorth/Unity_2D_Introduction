1edfb6e48ec1d15787dc2bc94d3a54c5359d545a
Danny North <lds.north@gmail.com>
2017-04-28 13:27:58 -0600

    Where we left off in the Unity Dev Guild on April 28th 2017

---
+++ 2D_Intro/Assets/Scripts/MovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+	private Rigidbody2D rigidbody;
+	private Animator animator;
+
+	public float speedFactor = 5f;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float walkingInput = Input.GetAxisRaw("Horizontal");
+
+		animator.SetBool("playerWalking", walkingInput != 0);
+		rigidbody.velocity = new Vector2(walkingInput * speedFactor, rigidbody.velocity.y);
+	}
+}
